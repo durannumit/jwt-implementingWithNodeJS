8c3c7db77a4408a54dfbd89e38b6dfc7d46ec5cd
umit <umit@umits-MacBook-Pro.local>
2019-05-14 13:07:17 +0300

    authorization login added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-//new 
 const Users = require('./data/users.json');
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); 
 //Endpoints
 app.get('/', (req, res) => {
   let dateTime = new Date();
@@ -12,6 +13,30 @@ app.get('/', (req, res) => {
     time: dateTime.getTime()
   });
 });
+//**** new post request function. ****
+ app.post('/auth/login', (req, res) => {
+  // validate if payload contains data
+   if (req.body.email && req.body.password) {
+//find the user in the json data
+     let findUser = Users.users.find((user) => {
+       return (user.email === req.body.email) ? user : false
+     });
+if (findUser) {
+       // send data
+       res.send(findUser);
+     } else {
+       res.status(404).send({
+         error: `Could not find user '${req.body.email}'.`
+       });
+     }
+   } else {
+     res.status(401).send({
+       error: `Invalid email and/or password.`
+     });
+   }
+ });
+
+
 //listen port
 app.listen(port);
 console.log(`Listening on port ${port}.`);
