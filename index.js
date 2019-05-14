a9e89c5be41957a34300e81e60f9f265ae121e09
umit <umit@umits-MacBook-Pro.local>
2019-05-14 13:06:16 +0300

    json created

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//imports
 const express = require('express');
 const app = express();
 const port = 5000;
-
+//new 
+const Users = require('./data/users.json');
+//Endpoints
 app.get('/', (req, res) => {
   let dateTime = new Date();
-
+ 
   res.send({
     time: dateTime.getTime()
   });
 });
-
+//listen port
 app.listen(port);
 console.log(`Listening on port ${port}.`);
